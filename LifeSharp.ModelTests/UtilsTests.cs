cd8776d7e7e353a34954f828ff89b725f78eaffd
Paul Tan <pjxtan@gmail.com>
2016-04-08 17:29:11 -0400

    Initial commit; defined test for getArrayFromFile()

---
+++ LifeSharp.ModelTests/UtilsTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LifeSharp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LifeSharp.Model.Tests
+{
+    [TestClass()]
+    public class UtilsTests
+    {
+        [TestMethod()]
+        [DeploymentItem("glider.txt")]
+        public void getArrayFromFileTest()
+        {
+            TextReader textReader = File.OpenText(@"glider.txt");
+
+            int[,] array = Utils.getArrayFromFile(textReader, 10, 10, " ");
+
+            int[,] expected =
+            {
+                { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 },
+                { 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            };
+
+            CollectionAssert.AreEqual(expected, array);
+        }
+    }
+}
