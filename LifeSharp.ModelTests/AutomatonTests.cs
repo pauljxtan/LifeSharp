cc588d6d2df2f12a043c7d679a058e1a6992773b
Paul Tan <pjxtan@gmail.com>
2016-04-08 15:59:26 -0400

    First commit; sparsely tested but stuff seems to work properly; no graphics yet

---
+++ LifeSharp.ModelTests/AutomatonTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LifeSharp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeSharp.Model.Tests
+{
+    [TestClass()]
+    public class AutomatonTests
+    {
+        [TestMethod()]
+        public void AutomatonTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EvolveTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ LifeSharp.ModelTests/Convolution2DTests.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LifeSharp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeSharp.Model.Tests
+{
+    [TestClass()]
+    public class Convolution2DTests
+    {
+        [TestMethod()]
+        public void ConvolveTest()
+        {
+            int[,] image =
+            {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+            };
+
+            int[,] kernel =
+            {
+                { 1, 3, 5 },
+                { 7, 9, 11 },
+                { 13, 15, 17 },
+            };
+
+            int[,] expected =
+            {
+                { 40,   91,  92 },
+                { 141, 285, 255 },
+                { 244, 433, 344 }
+            };
+
+            int[,] result = Convolution2D.Convolve(image, kernel);
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void PadImageWithZerosTest()
+        {
+            int[,] image =
+            {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+            };
+
+            int numRowsToPad = 1;
+            int numColsToPad = 2;
+
+            int[,] expected =
+            {
+                { 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 1, 2, 3, 0, 0 },
+                { 0, 0, 4, 5, 6, 0, 0 },
+                { 0, 0, 7, 8, 9, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0 }
+            };
+
+            int[,] imagePadded = Convolution2D.PadImageWithZeros(image, numRowsToPad, numColsToPad);
+
+            CollectionAssert.AreEqual(expected, imagePadded);
+        }
+
+        [TestMethod()]
+        public void FlipImageBothDimsTest()
+        {
+            int[,] image =
+            {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 },
+            };
+
+            int[,] expected =
+            {
+                { 9, 8, 7 },
+                { 6, 5, 4 },
+                { 3, 2, 1 }
+            };
+
+            int[,] imageFlipped = Convolution2D.FlipImageBothDims(image);
+
+            CollectionAssert.AreEqual(expected, imageFlipped);
+        }
+    }
+}
