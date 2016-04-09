966f39f4b63a9135598a9660cdd7ced4fe4ff74e
Paul Tan <pjxtan@gmail.com>
2016-04-09 19:02:29 -0400

    Added tests for constructing from size and initial seed

--- LifeSharp.ModelTests/GridConvolutionTests.cs
+++ LifeSharp.ModelTests/GridConvolutionTests.cs
@@ -12,6 +12,60 @@ namespace LifeSharp.Model.Tests
     public class GridConvolutionTests
     {
         [TestMethod()]
+        public void GridConvolutionFromSizeTest()
+        {
+            var grid = new GridConvolution(4, 2);
+
+            int[,] expected =
+            {
+                { 0, 0, 0, 0},
+                { 0, 0, 0, 0}
+            };
+
+            int[,] actual = grid.Cells;
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GridConvolutionWithSeedTest()
+        {
+            int[,] seed =
+            {
+                { 0, 1, 0, 0 },
+                { 0, 0, 1, 0 },
+                { 1, 1, 1, 0 },
+                { 0, 0, 0, 0 },
+            };
+
+            var grid = new GridConvolution(seed);
+
+            int[,] expectedCells =
+            {
+                { 0, 1, 0, 0 },
+                { 0, 0, 1, 0 },
+                { 1, 1, 1, 0 },
+                { 0, 0, 0, 0 }
+            };
+
+            int[,] actualCells = grid.Cells;
+
+            CollectionAssert.AreEqual(expectedCells, actualCells);
+
+            int[,] expectedLiveNeighbourCounts =
+            {
+                { 1, 1, 2, 1 },
+                { 3, 5, 3, 2 },
+                { 1, 3, 2, 2 },
+                { 2, 3, 2, 1 }
+            };
+
+            int[,] actualLiveNeighbourCounts = grid.LiveNeighbourCounts;
+
+            CollectionAssert.AreEqual(expectedLiveNeighbourCounts, actualLiveNeighbourCounts);
+        }
+
+        [TestMethod()]
         public void EvolveTest()
         {
             // Test a glider
