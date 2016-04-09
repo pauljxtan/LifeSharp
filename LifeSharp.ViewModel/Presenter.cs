26812655013768dd8b76ca656a817d85afefdcec
Paul Tan <pjxtan@gmail.com>
2016-04-09 12:55:42 -0400

    Initial commit; MVVM framework and presenter

---
+++ LifeSharp.ViewModel/Presenter.cs
@@ -0,0 +1,36 @@
+﻿using LifeSharp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace LifeSharp.ViewModel
+{
+    class Presenter : ObservableObject
+    {
+        private Automaton _automaton;
+
+        public Grid Universe
+        {
+            get
+            {
+                return _automaton.Universe;
+            }
+        }
+
+        public ICommand EvolveCommand
+        {
+            get
+            {
+                return new DelegateCommand(Evolve);
+            }
+        }
+
+        private void Evolve()
+        {
+            _automaton.Evolve();
+        }
+    }
+}
