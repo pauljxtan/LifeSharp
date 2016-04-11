f536c710e4e356f6ff8fdfd46ee7764f61ceccf3
Paul Tan <pjxtan@gmail.com>
2016-04-11 16:31:18 -0400

    Defined tests for Convert() and ConvertBack()

---
+++ LifeSharp.ViewModelTests/IntToColorConverterTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LifeSharp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Globalization;
+
+namespace LifeSharp.ViewModel.Tests
+{
+    [TestClass()]
+    public class IntToColorConverterTests
+    {
+        [TestMethod()]
+        public void ConvertTest()
+        {
+            var converter = new IntToColorConverter();
+
+            int value = 0;
+            Color expected = Colors.White;
+            Color actual = (Color)converter.Convert(value, typeof(Color), null, CultureInfo.CurrentCulture);
+
+            Assert.AreEqual(expected, actual);
+
+            value = 1;
+            expected = Colors.Black;
+            actual = (Color)converter.Convert(value, typeof(Color), null, CultureInfo.CurrentCulture);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ConvertBackTest()
+        {
+            var converter = new IntToColorConverter();
+
+            Color value = Colors.White;
+            int expected = 0;
+            int actual = (int)converter.ConvertBack(value, typeof(int), null, CultureInfo.CurrentCulture);
+
+            Assert.AreEqual(expected, actual);
+
+            value = Colors.Black;
+            expected = 1;
+            actual = (int)converter.ConvertBack(value, typeof(int), null, CultureInfo.CurrentCulture);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
