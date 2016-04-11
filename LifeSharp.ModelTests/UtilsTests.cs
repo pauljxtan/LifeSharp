1315164534e68ecda6cc2aa2a2d3a5d63deb2932
Paul Tan <pjxtan@gmail.com>
2016-04-11 18:05:29 -0400

    Defined test for ConvertArrayToCollection

--- LifeSharp.ModelTests/UtilsTests.cs
+++ LifeSharp.ModelTests/UtilsTests.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Collections.ObjectModel;
 
 namespace LifeSharp.Model.Tests
 {
@@ -35,6 +36,34 @@ namespace LifeSharp.Model.Tests
             };
 
             CollectionAssert.AreEqual(expected, array);
+        }
+        
+        [TestMethod()]
+        public void ConvertArrayToCollectionTest()
+        {
+            int[,] array =
+            {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+            };
+
+            var expected = new ObservableCollection<ObservableCollection<int>>();
+            expected.Add(new ObservableCollection<int>(new int[] { 1, 2, 3 }));
+            expected.Add(new ObservableCollection<int>(new int[] { 4, 5, 6 }));
+            expected.Add(new ObservableCollection<int>(new int[] { 7, 8, 9 }));
+
+            var actual = Utils.ConvertArrayToCollection(array);
+
+            Console.WriteLine(expected[0][0] + " " + expected[0][1] + " " + expected[0][2] + " " +
+                              expected[1][0] + " " + expected[1][1] + " " + expected[1][2] + " " +
+                              expected[2][0] + " " + expected[2][1] + " " + expected[2][2]);
+            Console.WriteLine(actual[0][0] + " " + actual[0][1] + " " + actual[0][2] + " " +
+                              actual[1][0] + " " + actual[1][1] + " " + actual[1][2] + " " +
+                              actual[2][0] + " " + actual[2][1] + " " + actual[2][2]);
+
+            // TODO: Why is this failing?
+            CollectionAssert.AreEqual(expected, actual);
         }
     }
 }
